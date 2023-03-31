aebe87f519db33a0e554c3f9102924f293dacd1f
thecml <chr1000@gmail.com>
2023-03-31 14:52:38 +0200

    Made version 0.1.0

--- setup.py
+++ setup.py
@@ -1,78 +1,22 @@
 from setuptools import setup
 
-######################################################################################################
-################ You May Remove All the Comments Once You Finish Modifying the Script ################
-######################################################################################################
-
 setup(
-    '''
-    This name will be used when people try to do pip install. And it is CASE SENSITIVE. 
-    You should create an unique name. Search on pypi.org to see if the name is taken or not.
-    '''
-    name = 'survival-analysis-datasets', 
-    
-    '''
-    The version number of your package consists of three integers "Major.Minor.Patch".
-    Typically, when you fix a bug, that will lead to a patch release. (e.g. 0.1.1 --> 0.1.2)
-    If you add a new feature to your package, that will lead to a minor release. (e.g. 0.1.0 --> 0.2.0)
-    If a major change that will affect many users happened, you will want to make it as a major release (e.g. 0.1.0 --> 1.0.0)
-    '''
+    name = 'survival-datasets', 
     version = '0.1.0',
-    
-    '''
-    This is the short description will show on the top of the webpage of your package on pypi.org
-    '''
-    description = 'Easy loader for datasets in Survival Analysis.',
-    
-    '''
-    This is the name of your main module file. No need to include the .py at the end.
-    '''    
-    py_modules = ["data_loader"],
-    
-    '''
-    Leave it as default. It shows where the module is stored.
-    '''
+    description = 'Data loader for common datasets in Survival Analysis.',
+    py_modules = ["survdata"],
     package_dir = {'':'src'},
-    
-    '''
-    If you have many modules included in your package, you want to use the following parameter instead of py_modules.
-    '''
-#     packages = ['ThePackageName1',
-#                 'ThePackageName2',
-#                 ...
-#  ],
-    
-    '''
-    Change the author name(s) and email(s) here.
-    '''
+    package_data = {'': ['data/*.feather', 'data/*.csv']}
     author = 'Christian Marius Lillelund',
     author_email = 'chr1000@gmail.com',
     
-    '''
-    Leave the following as default. It will show the readme and changelog on the main page of your package.
-    '''
-    long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+    long_description = open('README.md').read(),
     long_description_content_type = "text/markdown",
+
+    url='https://github.com/thecml/survival-datasets',
     
-    '''
-    The url to where your package is stored for public view. Normally, it will be the github url to the repository you just forked.
-    '''
-    url='https://github.com/thecml/survival-analysis-datasets',
-    
-    '''
-    Leave it as deafult.
-    '''
     include_package_data=True,
-    
-    '''
-    This is not a enssential part. It will not affect your package uploading process. 
-    But it may affect the discoverability of your package on pypi.org
-    Also, it serves as a meta description written by authors for users.
-    Here is a full list of what you can put here:
-    
-    https://pypi.org/classifiers/
-    
-    '''
+
     classifiers  = [
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
@@ -88,24 +32,14 @@ setup(
         'Operating System :: OS Independent',
     ],
     
-    
-    '''
-    This part specifies all the dependencies of your package. 
-    "~=" means the users will need a minimum version number of the dependecies to run the package.
-    If you specify all the dependencies here, you do not need to write a requirements.txt separately like many others do.
-    '''
     install_requires = [
-
-        'pandas ~= 1.2.4',
-        ...
-
+        'scikit-survival ~= 0.17.2',
+        'pandas ~= 1.4.3',
+        'numpy ~= 1.22.4',
+        'shap ~= 0.41.0',
+        'pyarrow ~= 11.0.0',
     ],
     
-    
-    
-    '''
-    The keywords of your package. It will help users to find your package on pypi.org
-    '''
-    keywords = ['Survival Analysis', 'Machine Learning', 'Datasets'],
+    keywords = ['Survival Analysis', 'Datasets'],
     
 )
---
+++ src/survdata.py
@@ -0,0 +1,106 @@
+__author__='Christian Marius Lillelund'
+__author_email__='chr1000@gmail.com'
+
+import numpy as np
+import pandas as pd
+import shap
+from sksurv import datasets
+
+import pkg_resources
+
+resource_package = __name__
+
+def convert_to_structured(T, E):
+    default_dtypes = {"names": ("event", "time"), "formats": ("bool", "f8")}
+    concat = list(zip(E, T))
+    return np.array(concat, dtype=default_dtypes)
+
+def load_seer_dataset():
+    resource_path = '/'.join(('data', 'seer.csv'))
+    resource = pkg_resources.resource_stream(resource_package, resource_path)    
+    data = pd.read_csv(resource)
+
+    outcomes = data.copy()
+    outcomes['event'] =  data['Status']
+    outcomes['time'] = data['Survival Months']
+    outcomes = outcomes[['event', 'time']]
+    outcomes.loc[outcomes['event'] == 'Alive', ['event']] = 0
+    outcomes.loc[outcomes['event'] == 'Dead', ['event']] = 1
+    
+    data = data.drop(['Status', "Survival Months"], axis=1)
+    
+    X = pd.DataFrame(data)
+    y = convert_to_structured(outcomes['time'], outcomes['event'])
+    return (X, y)
+
+def load_nhanes_dataset():    
+    nhanes_X, nhanes_y = shap.datasets.nhanesi()
+    X = pd.DataFrame(nhanes_X)
+    event = np.array([True if x > 0 else False for x in nhanes_y])
+    time = np.array(abs(nhanes_y))
+    y = convert_to_structured(time, event)
+    return (X, y)
+
+def load_support_dataset():
+    resource_path = '/'.join(('data', 'support.feather'))
+    resource = pkg_resources.resource_stream(resource_package, resource_path)    
+    data = pd.read_feather(resource)
+
+    outcomes = data.copy()
+    outcomes['event'] =  data['event']
+    outcomes['time'] = data['duration']
+    outcomes = outcomes[['event', 'time']]
+    
+    feats =  ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6',
+              'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13']
+    
+    X = pd.DataFrame(data[feats])
+    y = convert_to_structured(outcomes['time'], outcomes['event'])
+    return (X, y)
+
+def load_aids_dataset():
+    X, y = datasets.load_aids()
+    X = pd.DataFrame(X) 
+    y = convert_to_structured(y['time'], y['censor'])
+    return (X, y)
+
+def load_veterans_dataset():
+    X, y = datasets.load_veterans_lung_cancer()
+    X = pd.DataFrame(X) 
+    y = convert_to_structured(y['Survival_in_days'], y['Status'])
+    return (X, y)
+
+def load_whas500_dataset():
+    X, y = datasets.load_whas500()
+    X = pd.DataFrame(X) 
+    y = convert_to_structured(y['lenfol'], y['fstat'])
+    return (X, y)
+
+def load_flchain_dataset():
+    X, y = datasets.load_flchain()
+    X = pd.DataFrame(X) 
+    y = convert_to_structured(y['futime'], y['death'])
+    return (X, y)
+
+def load_gbsg2_dataset():
+    X, y = datasets.load_gbsg2()
+    X = pd.DataFrame(X) 
+    y = convert_to_structured(y['time'], y['cens'])
+    return (X, y)
+
+def load_metabric_dataset():
+    resource_path = '/'.join(('data', 'metabric.feather'))
+    resource = pkg_resources.resource_stream(resource_package, resource_path)    
+    data = pd.read_feather(resource)
+
+    outcomes = data.copy()
+    outcomes['event'] =  data['event']
+    outcomes['time'] = data['duration']
+    outcomes = outcomes[['event', 'time']]
+
+    num_feats =  ['x0', 'x1', 'x2', 'x3', 'x8'] \
+                    + ['x4', 'x5', 'x6', 'x7']
+
+    X = pd.DataFrame(data[num_feats])
+    y = convert_to_structured(outcomes['time'], outcomes['event'])
+    return (X, y)
